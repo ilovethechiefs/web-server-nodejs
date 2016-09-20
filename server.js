25b307161c47d790bd39108c4889132c798ce6b6
Philip King <ilovethechiefs@gmail.com>
2016-09-20 16:25:28 -0500

    Initialize repo - Second time through lesson

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var app = express();
-var port = 3000;
-var middleware = require('./middleware');
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	}, 
+	logger: function (req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' - ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
 
 app.use(middleware.logger);
 
-// app.use(middleware.requireAuthentication);
-
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About Us');
+	res.send("About Us");
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function() {
-	console.log('Express server started on port ' + port + '!');
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
 });
